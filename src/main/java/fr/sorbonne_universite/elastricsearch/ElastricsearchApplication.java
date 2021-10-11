6d0705c730082895873230ad59cf0e65416e8123
knhaila <nhailakaoutar@gmail.com>
2021-10-11 12:12:06 +0100

    Intial Project

---
+++ src/main/java/fr/sorbonne_universite/elastricsearch/ElastricsearchApplication.java
@@ -0,0 +1,13 @@
+package fr.sorbonne_universite.elastricsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElastricsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElastricsearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/sorbonne_universite/elastricsearch/ElastricsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sorbonne_universite.elastricsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElastricsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
